78520a45e79157dcd98e78270789ca2456035d62
greenMakaroni <mrtn.dawid@gmail.com>
2023-07-20 13:54:00 +0100

    added drei helpers, camera controls and gizmo helper

--- src/components/home/banner/Scene.jsx
+++ src/components/home/banner/Scene.jsx
@@ -1,15 +1,28 @@
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Dmu } from './Dmu'
-
+import { CameraControls, GizmoHelper, GizmoViewport } from '@react-three/drei'
 const Scene = () => {
 
   return (
     <div className="canvas">
       <Canvas >
+        {/* Lights */}
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
+
+        {/* Models */}
         <Dmu />
+
+        {/* Drei helpers */}
+        <CameraControls />
+        <GizmoHelper
+          alignment="bottom-right" // widget alignment within scene
+          margin={[80, 80]} // widget margins (X, Y)
+        >
+          <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor="black" />
+          {/* alternative: <GizmoViewcube /> */}
+        </GizmoHelper>
       </Canvas>
     </div>
 
