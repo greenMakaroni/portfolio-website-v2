c0ce209fd30f97efd05d12ccea67ccc070d275f6
greenMakaroni <mrtn.dawid@gmail.com>
2023-07-24 16:27:07 +0100

    converted skills css to tailwind

--- src/components/home/skills/Skills.jsx
+++ src/components/home/skills/Skills.jsx
@@ -1,8 +1,7 @@
-import "./skills.css"
 
 const Skills = () => {
   return (
-    <div className="skills-main">Skills</div>
+    <div className="w-screen h-screen p-0 m-0 flex flex-row justify-start">Skills</div>
   )
 }
 
