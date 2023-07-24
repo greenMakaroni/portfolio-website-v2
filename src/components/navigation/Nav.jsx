b55eb44def9b68386bee456a3fec0f5e2da90930
greenMakaroni <mrtn.dawid@gmail.com>
2023-07-24 16:12:13 +0100

    converted nav css to tailwind

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -3,8 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 import useScrollPosition from "../../custom_hooks/useScrollPosition"; 
 import useWindowDimensions from '../../custom_hooks/useWindowDimensions'
 
-import "./nav.css";
-
 export default function Nav() {
   const location = useLocation();
   const { height, width } = useWindowDimensions();
@@ -31,13 +29,14 @@ export default function Nav() {
   }, [scrollPosition.prev])
 
   return (
-    <nav className={show ? "nav" : "nav-hidden"}>
-      <div className="links">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}> Home </Link>
-        <Link to="/education" className={location.pathname === "/education" ? "nav-link active" : "nav-link"}> Education </Link>
-        <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}> Portfolio </Link>
-        <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}> Contact </Link>
+    <nav className={`fixed flex flex-row align-center w-screen h-[60px] bg-white/[0] pt-[20px] pb-[20px] select-none duration-100 top-0 right-0 overflow-hidden z-10 ${show ? "opacity-100" : "opacity-0"}`}>
+      <div className="flex flex-row m-0 p-0 w-screen justify-center">
+        <Link to="/" className={`mr-[30px] p-0 text-base text-black font-['Geologica'] font-thin ${location.pathname === "/" ? "underline decoration-black decoration-2 underline-offset-4" : "no-underline" }`}> Home </Link>
+        <Link to="/education" className={`mr-[30px] p-0 text-base text-black font-['Geologica'] font-thin ${location.pathname === "/education" ? "underline decoration-black decoration-2 underline-offset-4" : "no-underline" }`}> Education </Link>
+        <Link to="/portfolio" className={`mr-[30px] p-0 text-base text-black font-['Geologica'] font-thin ${location.pathname === "/portfolio" ? "underline decoration-black decoration-2 underline-offset-4" : "no-underline" }`}> Portfolio </Link>
+        <Link to="/contact" className={`mr-[30px] p-0 text-base text-black font-['Geologica'] font-thin ${location.pathname === "/contact" ? "underline decoration-black decoration-2 underline-offset-4" : "no-underline" }`}> Contact </Link>
       </div>
     </nav>
   )
 }
+
