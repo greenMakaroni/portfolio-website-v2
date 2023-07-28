0c0bbc328c31cfa17ee03a4ce224f67ac4cfcf33
greenMakaroni <mrtn.dawid@gmail.com>
2023-07-28 14:42:45 +0100

    deleted unsused animation keyframes

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,7 +3,6 @@ import Navigation from '../../components/navigation/Navigation'
 const Contact = () => {
   return (
     <>
-      <div className="w-screen h-screen p-0 m-0 z-10 absolute bg-[#ff8800] animate-shortTransition"></div>
       <Navigation />
     </>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,17 +11,9 @@ export default {
     },
     extend: {
       keyframes: {
-        'shortTransition': {
-          '0%': { left: '0'},
-          '100%': {  left: '-105vw' },
-        },
         'moveCanvas': {
           '0%': {marginRight: '0'},
           '100%': {marginRight: '-15vw'}
-        },
-        'showNav': {
-          '0%': {opacity: '0'},
-          '100%': {opacity: '1'}
         },
         'elementIn': {
           '0%': {opacity: '0', marginLeft: '-5vw'},
@@ -29,9 +21,7 @@ export default {
         }
       },
       animation: {
-        shortTransition: 'shortTransition ease-in-out 0.7s forwards',
         moveCanvas: '2s moveCanvas ease-in-out 1s forwards',
-        showNav: 'showNav ease 0.3s forwards',
         infoElementIn: '1s elementIn ease 1.6s forwards',
         elementIn: 'elementIn 1s forwards'
       },
