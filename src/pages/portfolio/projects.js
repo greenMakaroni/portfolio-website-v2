243c5029adbf4d162026f06fbd5e8c62210ee4cd
greenMakaroni <mrtn.dawid@gmail.com>
2024-09-02 15:33:31 +0100

    delete redundant files

--- src/pages/portfolio/projects.js
+++ src/pages/portfolio/projects.js
@@ -1,6 +1,6 @@
 const projects = [
   {
-    title: "my notes",
+    title: "My notes",
     description:
       "Full stack web application for creating and managing notes. Project created using PHP, Laravel, Tailwind and MySQL database.",
     link: "https://github.com/greenMakaroni/myNotes",
@@ -8,7 +8,7 @@ const projects = [
     date: "November 2023",
   },
   {
-    title: "interactive rating component",
+    title: "Interactive rating component",
     description:
       "This is my solution to the Interactive rating component challenge found on www.frontendmentor.io",
     link: "https://greenmakaroni.github.io/Interactive-rating-component/",
@@ -16,7 +16,7 @@ const projects = [
     date: "August 2023",
   },
   {
-    title: "project management app",
+    title: "Project management app",
     description:
       "Full stack application to manage projects and clients. React, apollo client, and bootstrap on the front end. GraphQL, MongoDB, express on the back end.",
     link: "https://github.com/greenMakaroni/project-management-app.git",
@@ -24,7 +24,7 @@ const projects = [
     date: "July 2022",
   },
   {
-    title: "java module chooser",
+    title: "Java module chooser",
     description:
       "MVC desktop application written in Java for choosing optional modules for the final year at the university. GUI created using javafx library. ",
     link: "https://github.com/greenMakaroni/Module-Chooser-GUI",
@@ -32,7 +32,7 @@ const projects = [
     date: "December 2021",
   },
   {
-    title: "e-commerce store",
+    title: "E-commerce store",
     description:
       "eCommerce storefront written in React, utilizing commerce.js API for back end functionality, and stripe for processing of payments.",
     link: "https://github.com/greenMakaroni/react-commercejs-storefront",
